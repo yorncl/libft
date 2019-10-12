f5e9c9b3691f27720dbe7617c845d960dd31152b
Martin CLAUDEL <mclaudel@e2r4p14.42.fr>
2019-10-12 19:09:18 +0200

    I think it's time to push

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mclaudel <mclaudel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/10 11:08:15 by mclaudel          #+#    #+#             */
-/*   Updated: 2019/10/12 18:40:56 by mclaudel         ###   ########.fr       */
+/*   Updated: 2019/10/12 19:05:07 by mclaudel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*dest;
 	size_t	i;
 
-	if (start > ft_strlen(s))
-		return (0);
 	if (!(dest = (ft_calloc(1, len + 1))))
 		return (0);
 	i = -1;
-	while (s[++i] && i < len)
-		dest[i] = s[start + i];
+	if (start < ft_strlen(s))
+		while (s[++i] && i < len)
+			dest[i] = s[start + i];
 	return (dest);
 }
