10076f3d9da2b014eac3e494c5611c1f8a2934ab
mclaudel <mclaudel@student.42.fr>
2020-06-03 00:43:17 +0200

    fixed a dictrem error

--- ft_dictrem.c
+++ ft_dictrem.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/26 14:35:24 by user42            #+#    #+#             */
-/*   Updated: 2020/05/26 14:36:07 by user42           ###   ########.fr       */
+/*   Updated: 2020/06/03 00:42:48 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,12 @@ void	ft_dictrem(t_dict **dict, char *key, void (*del)(void *))
 	{
 		torem = *dict;
 		*dict = (*dict)->next;
+		ft_dictdelone(torem, del);
 	}
 	else
 	{
 		curr = *dict;
 		while (curr->next)
-		{
 			if (ft_strcmp(curr->next->key, key) == 0)
 			{
 				torem = curr->next;
@@ -36,7 +36,7 @@ void	ft_dictrem(t_dict **dict, char *key, void (*del)(void *))
 				ft_dictdelone(torem, del);
 				return ;
 			}
-			curr = curr->next;
-		}
+			else
+				curr = curr->next;
 	}
 }
